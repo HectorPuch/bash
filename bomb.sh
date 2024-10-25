9f7170119ff839ca2d231f6ae84ab79022b9c8bf
Hector Puch <hectorpuchortonoves@gmail.com>
2024-10-25 17:40:10 +0200

    exercise01_bash

---
+++ bomb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo"¿Desde que número quieres que empiece?" | cowsay
+# Esto es un comentario
+read CANTIDAD
+
+# for NUMERO in $(seq 10) para matemáticas
+for NUMERO in `seq 0 $CANTIDAD | sort -hr`
+do
+	clear
+	echo $NUMERO | cowsay
+	sleep 1
+done
+
+clear
+echo "BOOOOM!" | cowsay d
